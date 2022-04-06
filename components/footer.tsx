227bbd7d2763e10fd53d8f7b1e6a07833e9ca2f8
brianreworded <94920515+brianreworded@users.noreply.github.com>
2022-04-06 17:02:08 -0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <div>
     <footer className="text-center font-mainfont text-th-fonting bg-th-background mt-3">
-    <a href="mailto:brian@reworded.io" className='hover:text-reworded'>Email</a>&nbsp;&nbsp; | &nbsp;<a href="https://www.linkedin.com/in/brian-swanberg/" className='hover:text-reworded'> LinkedIn </a>&nbsp; | &nbsp;<a href="https://twitter.com/brianswanberg/" className='hover:text-reworded'> Twitter</a>  &nbsp;&nbsp;| &nbsp; <a href="https://www.github.com" className='hover:text-reworded'> GitHub Repository </a>&nbsp;
+    <a href="mailto:brian@reworded.io" className='hover:text-reworded'>Email</a>&nbsp;&nbsp; | &nbsp;<a href="https://www.linkedin.com/in/brian-swanberg/" className='hover:text-reworded'> LinkedIn </a>&nbsp; | &nbsp;<a href="https://twitter.com/brianswanberg/" className='hover:text-reworded'> Twitter</a>  &nbsp;&nbsp;| &nbsp; <a href="https://www.github.com/brianreworded/brian-portfolio" className='hover:text-reworded'> GitHub Repository </a>&nbsp;
          <br/>
         Copyright © 2022 Brian Swanberg.
     </footer>
