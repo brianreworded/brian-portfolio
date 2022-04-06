a646ad037eb8c65069a50e2622296ee064ff0a52
brianreworded <94920515+brianreworded@users.noreply.github.com>
2022-04-06 17:08:21 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,21 @@ const Home: NextPage = () => {
   return (
     <div className='font-mainfont text-th-fonting'>
 
-      <h1 className='text-5xl text-center mt-8 sm-text-xl'>Hi, I'm Brian Swanberg.
+      <h1 className='text-5xl text-center mt-8 sm-text-xl'>Hi, I&apos;m Brian Swanberg.
       <br/>
       <br/>
       <br/>
       </h1>
 
       <p className='mx-[15%] text-xl sm-text-m'>
-          I'm a data scientist and software engineer interested in leveraging data to help improve quality of life, whether it would be through biotechnology or education. 
+          I&apos;m a data scientist and software engineer interested in leveraging data to help improve quality of life, whether it would be through biotechnology or education. 
           <br/>
           <br/>
           Previously I double-majored in Data Science and Biology at Minerva University, an innovative university where students typically study in 7 countries over 4 years.
           <br/>
           <br/>
           
-          Currently, I'm building <a href="reworded.io"><span className="font-dyslexia text-reworded">reworded</span> </a>, the premier AI-based reading comprehension assistant.
+          Currently, I&apos;m building <a href="reworded.io"><span className="font-dyslexia text-reworded">reworded</span> </a>, the premier AI-based reading comprehension assistant.
           <br/>
           <br/>
           My goal: improve the quality of life of 1 million people by May 2031.
