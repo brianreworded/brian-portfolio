0819f8d336b8a6f2406b92910774563d59a6f30e
brianreworded <94920515+brianreworded@users.noreply.github.com>
2022-04-22 05:53:31 -0400

    Add files via upload

--- pages/bucket-list.tsx
+++ pages/bucket-list.tsx
@@ -36,8 +36,8 @@ const BucketList: NextPage = () => {
         <li className='mt-3 line-through'>&bull; Walk inside the Library of Alexandria</li>
         <li className='mt-3'>&bull; Become a professional pickleball player</li>
         <li className='mt-3 line-through'>&bull; Try ice cream and pizza in Italy</li>
-        <li className='mt-3'> &bull; Re-create the best chicken shawarma from Hyderabad</li>  
-        <li className='mt-3'>&bull; Become pescatarian</li>
+        <li className='mt-3'> &bull; Re-create the best chicken shawarma from Hyderabad </li>  
+        <li className='mt-3'>&bull; Become pescatarian for 1 year</li>
         <li className='mt-3 '> &bull; Become fluent in Spanish</li>
         <li className='mt-3 '> &bull; Celebrate the miracle that is life at Machu Picchu</li>
       </ul>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,14 @@ const Home: NextPage = () => {
       </h1>
 
       <p className='mx-[15%] text-xl sm-text-m'>
-          I&apos;m a data scientist and software engineer interested in leveraging data to help improve quality of life, whether it would be through biotechnology or education. 
+          I&apos;m a data scientist and software engineer interested in leveraging data to help improve quality of life, whether through biotechnology, health care, or education. I have a product-oriented focus, and recently, I have focused on innovations that quantify natural language.
           <br/>
           <br/>
-          Previously I double-majored in Data Science and Biology at Minerva University, an innovative college where students typically study in 7 countries over 4 years.
+          Previously I double-majored in Data Science and Biology at <a href="https://www.minerva.edu/undergraduate-program/" className='text-th-fontingsecondary hover:text-reworded'> Minerva University</a>, an innovative college where students typically study in 7 countries over 4 years.
           <br/>
           <br/>
           
-          Currently, I&apos;m building <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded">reworded</span> </a>, the premier AI-based reading comprehension assistant. I also built other products, like a multilingual text shortener and a <a href="https://nointextcitations.herokuapp.com/" className="text-th-fontingsecondary">specialized word counter</a> for my college classmates at Minerva.
+          Currently, I&apos;m building <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span> </a>, the premier AI-based reading comprehension assistant. I also built other products, like a multilingual text shortener and a <a href="https://nointextcitations.herokuapp.com/" className="text-th-fontingsecondary hover:text-reworded">specialized word counter</a> for my college classmates at Minerva.
           <br/>
           <br/>
           My goal: improve the quality of life of 1 million people by May 2031.
