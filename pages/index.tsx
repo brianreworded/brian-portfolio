1446a2c1674db30e089eb25848881a3f7bf967f8
brianreworded <94920515+brianreworded@users.noreply.github.com>
2022-10-07 08:34:44 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           <br/>
           <br/>
           
-          Currently, I&apos;m building <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span> </a>, the premier AI-based reading comprehension assistant. I also created other products, like a <a href="https://reduction.lol" className="text-th-fontingsecondary hover:text-reworded">multilingual text shortener</a> for ESL college students and a <a href="https://nointextcitations.herokuapp.com/" className="text-th-fontingsecondary hover:text-reworded">specialized word counter</a> for my college classmates at Minerva.
+          Currently, I&apos;m building <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span> </a>, the premier AI-based reading comprehension assistant. I also created other products, like a <a href="https://reduction.ai" className="text-th-fontingsecondary hover:text-reworded">multilingual text shortener</a> for ESL college students and a <a href="https://nointextcitations.herokuapp.com/" className="text-th-fontingsecondary hover:text-reworded">specialized word counter</a> for my college classmates at Minerva.
           <br/>
           <br/>
           My goal: improve the quality of life of 1 million people by May 2031.
