0264d52e32a16d9931df3e8fefeb4db7b0494067
brianreworded <brian@reworded.io>
2023-11-18 18:54:46 -0600

    update webpage copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,8 +20,7 @@ const Home: NextPage = () => {
           Previously I double-majored in Data Science and Biology at <a href="https://www.minerva.edu/undergraduate-program/" className='text-th-fontingsecondary hover:text-reworded'> Minerva University</a>, an innovative college where students typically study in 7 countries over 4 years.
           <br/>
           <br/>
-          
-          Currently, I&apos;m spearheading data science development at <a href="https://www.mentra.me" className='text-th-fontingsecondary hover:text-reworded'>Mentra</a>, whose mission is to tap into the potential of 1 billion neurodivergents worldwide. I&apos;m also building a premium multilingual text shortener in stealth, which is currently under a test period in schools. Previously I created <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span> </a>, the premier AI-based reading comprehension assistant, along with many other NLP-focused or decision-inference applications. 
+          Currently, I&apos;m spearheading data science development at <a href="https://www.mentra.com" className='text-th-fontingsecondary hover:text-reworded'>Mentra</a>, whose mission is to tap into the potential of 1 billion neurodivergents worldwide. Previously I created <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span> </a>, the premier AI-based reading comprehension assistant, and a <a href="https://www.reduction.ai" className='text-th-fontingsecondary hover:text-reworded'>site for auto-generated Sparknotes</a>.
         <br/>  
         <br/>
           My goal: improve the quality of life of 1 million people by May 2031.
