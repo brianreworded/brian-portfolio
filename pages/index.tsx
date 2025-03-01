e8342a2ff9f7d1db90c9c73b64314cfe09b12768
brianreworded <brian.swanberg@gmail.com>
2025-02-28 19:44:06 -0600

    update work history on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,18 @@ const Home: NextPage = () => {
       </h1>
 
       <p className='mx-[15%] text-xl sm-text-m'>
-          I&apos;m a data scientist and software engineer interested in leveraging data to help improve quality of life. I have a product-oriented focus, and recently, I have focused on innovations that quantify natural language.
+          I&apos;m a data scientist interested in leveraging data to help improve quality of life. I have a product-oriented focus, and in the past, I have focused on innovations that quantify natural language.
           <br/>
           <br/>
-          Previously I double-majored in Data Science and Biology at <a href="https://www.minerva.edu/undergraduate-program/" className='text-th-fontingsecondary hover:text-reworded'> Minerva University</a>, an innovative college where students typically study in 7 countries over 4 years.
-          <br/>
-          <br/>
-          Currently, I&apos;m figuring out what is next!
+          Currently, I&apos;m an AI Engineer at <a href="https://www.quantified.ai" className='text-th-fontingsecondary hover:text-reworded'>Quantified</a>, building on a vision I had with a project, <a href="https://jumpmail.vercel.app" className='text-th-fontingsecondary hover:text-reworded'>Jumpmail</a>.
           <br /> <br />
-          From January 2023 through August 2024, I spearheaded data science development at a seed stage startup, <a href="https://unlocked.microsoft.com/mentra-neuroinclusion/" className='text-th-fontingsecondary hover:text-reworded'>Mentra</a>, trying to help tap into the potential of neurodivergents. Prior, before the world of ChatGPT, I built <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span></a>, the premier AI-based reading comprehension assistant, and a <a href="https://www.reduction.ai" className='text-th-fontingsecondary hover:text-reworded'>site for auto-generated Sparknotes</a>.
+          Previously, I spearheaded data science development at a seed stage startup, <a href="https://unlocked.microsoft.com/mentra-neuroinclusion/" className='text-th-fontingsecondary hover:text-reworded'>Mentra</a>, trying to help tap into the potential of neurodivergents. Prior, before the world of ChatGPT, I built <a href="https://www.reworded.io"><span className="font-dyslexia text-reworded hover:text-th-fontingsecondary">reworded</span></a>, the premier AI-based reading comprehension assistant, and a <a href="https://www.reduction.ai" className='text-th-fontingsecondary hover:text-reworded'>site for auto-generated Sparknotes</a>.
         <br/>  
         <br/>
-          My goal: improve the quality of life of 1 million people by May 2031.
+        I&apos;m a proud alum of the innovative and global university, <a href="https://www.minerva.edu/undergraduate-program/" className='text-th-fontingsecondary hover:text-reworded'> Minerva</a>, where I studied biology and data science.
+        <br/>
+        <br/>
+          My current end goal: improve the quality of life of 1 million people by May 2031.
 
           <br/>
           <br/>
